3a73f25e8d116b5dbf9a34eb771d13abe742281e
sweet915 <645586550@qq.com>
2026-09-16 21:13:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reading-hero-v1';
+const CACHE = 'reading-hero-v2';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
